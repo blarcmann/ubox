e4ad141acf1972f720e6456d76d391bbdaaa370d
Odetola Azeez Opeyemi <yemiOtola@gmail.com>
2021-04-07 11:26:01 +0100

    :sparkles: testing PR

--- src/components/video/likeDislike.js
+++ src/components/video/likeDislike.js
@@ -5,6 +5,8 @@ import { DislikeFilled, LikeFilled } from '@ant-design/icons';
 
 export default function LikeDislike(props) {
   const { videoId, video } = props;
+  const [lk, setLk] = useState(false);
+  const [dlk, setDlk] = useState(false);
   const user = JSON.parse(localStorage.getItem('auth'));
   const dispatch = useDispatch();
 
@@ -19,25 +21,27 @@ export default function LikeDislike(props) {
   const disliked = useSelector(state => state.video.disliked);
 
   const onLike = () => {
+    setDlk(false);
+    setLk(true)
     dispatch(like(payload));
   }
 
   const onDislike = () => {
+    setLk(false)
+    setDlk(true);
     dispatch(dislike(payload))
   }
 
-  console.log(liked, disliked)
-
   return (
     <div className="like-dislike">
-      <div className="Like">
-        <LikeFilled onClick={onLike} className={liked ? 'liked' : null} />
-        <span>{likes && likes.length > 0 ? likes.length : 0}</span>
+      <div className={liked || lk ? 'dl liked' : 'dl'}>
+        <LikeFilled onClick={onLike} />
+        <span className="number">{likes && likes.length > 0 ? `${likes.length}k` : 0}</span>
       </div>
 
-      <div className="Like">
-        <DislikeFilled onClick={onDislike} className={disliked ? 'dislike' : null} />
-        <span>{dislikes && dislikes.length > 0 ? dislikes.length : 0}</span>
+      <div className={disliked || dlk ? 'dl disliked' : 'dl'}>
+        <DislikeFilled onClick={onDislike} />
+        <span className="number">{dislikes && dislikes.length > 0 ? dislikes.length : 0}</span>
       </div>
 
     </div>
